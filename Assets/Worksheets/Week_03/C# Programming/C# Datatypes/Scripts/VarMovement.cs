6bf64d6798d3cae790b07e755e9543b83ce561a3
Hong Yang <107310534+Imadeanaccounttoeditstuff@users.noreply.github.com>
2025-05-09 11:14:57 +0800

    some pushing

--- Assets/Worksheets/Week_03/C# Programming/C# Datatypes/Scripts/VarMovement.cs
+++ Assets/Worksheets/Week_03/C# Programming/C# Datatypes/Scripts/VarMovement.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class VarMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        var speed = 5.0f; // Inferred as float
+        var direction = new Vector3(1, 0, 0); // Inferred as Vector3
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        // speed = "fast"; 
     }
 }
